5705ac75d018197f4f5586d899ce181108b837e2
lovebird <lovebirdsx@gmail.com>
2026-04-04 12:19:40 +0800

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,25 @@
+import { Command as CommanderCommand } from "commander";
+
 async function run() {
-    console.log('Hello, World!');
+    const program = new CommanderCommand();
+    program.hook('preAction', async () => {
+        console.log('Running preAction hook...');
+    });
+
+    program.name('xplay')
+    .description('A CLI tool for XPlay')
+    .version('0.1.0')
+    .helpOption('-h, --help', 'Display help for command')
+    .argument('[prompt]', 'Your prompt', String)
+    .option('-v, --verbose', 'Enable verbose mode')
+    .action(async (prompt, options) => {
+        console.log(`You entered: ${prompt}`);
+        if (options.verbose) {
+            console.log('Verbose mode is enabled');
+        }
+    });
+
+    program.parse(process.argv);
 }
 
 export async function main() {
